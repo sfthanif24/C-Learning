ea5565eb36d359123efb912d3c084701dbd4d2d5
SHT <mohammadhanif8664@gmail.com>
2025-11-11 23:37:40 +0800

    Add files via upload

---
+++ 65_series01.c
@@ -0,0 +1,16 @@
+// 1 + 2 + 3 + .... + n series
+#include <stdio.h>
+int main()
+{
+    int n, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
